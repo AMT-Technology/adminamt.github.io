a9e05156092355d45b16f06c9b3bb5c3215d9fb6
AMT-Technology <des.amt.01@gmail.com>
2025-12-29 15:45:18 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Configuración Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCEMOAiEhDC-Z5nS4_yB-a1s3T_61PrHF4"
+  authDomain: "appwebs-8b23f.firebaseapp.com",
+  projectId: "appwebs-8b23f",
+  storageBucket: "appwebs-8b23f.firebasestorage.app",
+  messagingSenderId: "986164552498",
+  appId: "1:986164552498:web:04262bde6fa35e5efced2b",
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Servicios
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
